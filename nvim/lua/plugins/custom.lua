81e8625c6e7e2f55f9923d4891e875f950cb4768
Haseeb Majid <hello@haseebmajid.dev>
2023-04-16 19:46:12 +0100

    Update nvim config

--- nvim/lua/plugins/custom.lua
+++
@@ -1,3 +0,0 @@
-return {
-  {'nvim-pack/nvim-spectre'},
-}
---
+++ nvim/lua/user/plugins/user.lua
@@ -0,0 +1,22 @@
+return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  {
+    'maxmx03/dracula.nvim',
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      local dracula = require('dracula')
+
+      dracula.setup({})
+    end
+  }
+}
