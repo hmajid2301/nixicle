4a0a5acea92735501ec654f5c39d1ac5a507fc00
Haseeb Majid <hello@haseebmajid.dev>
2023-04-17 11:09:05 +0000

    feat: Add MacOS Config

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,9 @@
-starship init fish | source
-set -x PATH $PATH $HOME/.pyenv/bin $HOME/.poetry/bin $HOME/.local/bin $HOME/go/bin
+set PATH $HOME/.pyenv/bin $HOME/.poetry/bin $HOME/.local/bin $HOME/go/bin /usr/local/bin $PATH
 set -x GOPATH $HOME/go
 set -x EDITOR vim
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ]
-    . "$HOME/google-cloud-sdk/path.fish.inc"
-end
-
+starship init fish | source
 zoxide init fish | source
-
-# The next line updates PATH for Netlify's Git Credential Helper.
-test -f '/home/haseeb/.config/netlify/helper/path.fish.inc' && source '/home/haseeb/.config/netlify/helper/path.fish.inc'
-# pnpm
-set -gx PNPM_HOME "/home/haseeb/.local/share/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-alias pn=pnpm
-# pnpm end
-
 source /opt/asdf-vm/asdf.fish
+source ~/.config/fish/extra.fish
+
---
+++ fish/extra.fish
@@ -0,0 +1,20 @@
+# pyenv init
+if command -v pyenv 1>/dev/null 2>&1
+    pyenv init - | source
+end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ]
+    . "$HOME/google-cloud-sdk/path.fish.inc"
+end
+
+# The next line updates PATH for Netlify's Git Credential Helper.
+test -f "$HOME/.config/netlify/helper/path.fish.inc" && source '/home/haseeb/.config/netlify/helper/path.fish.inc'
+
+# pnpm
+set -gx PNPM_HOME "/home/haseeb/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+alias pn=pnpm
+# pnpm end
+
+bass source /usr/local/bin/source-aliases
