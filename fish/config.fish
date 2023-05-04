b10b2a1ad9786683ff344e672e13fa725737ad89
Haseeb Majid <hello@haseebmajid.dev>
2023-05-04 23:14:44 +0100

    Remove done.fish

--- fish/config.fish
+++ fish/config.fish
@@ -11,8 +11,8 @@ fish_vi_key_bindings
 bind --mode insert --sets-mode default jk repaint
 
 export FZF_DEFAULT_OPTS="
---color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 
---color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf 
+--color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284
+--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf
 --color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284
 "
 
