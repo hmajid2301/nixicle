0a5b05f58276d1af067a5daa9312a2949fa74c30
Haseeb Majid <hello@haseebmajid.dev>
2023-07-27 16:09:56 +0000

    Update Packages

--- home-manager/editors/nvim/config/lua/plugins/coding.lua
+++ home-manager/editors/nvim/config/lua/plugins/coding.lua
@@ -36,4 +36,35 @@ return {
 			mappings = { toggle = "" },
 		},
 	},
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/neotest-go",
+		},
+		keys = {
+			{ "<leader>ta", "<cmd>lua require('neotest').run.attach()<cr>", desc = "Attach to the nearest test" },
+			{ "<leader>tl", "<cmd>lua require('neotest').run.run_last()<cr>", desc = "Toggle Test Summary" },
+			{
+				"<leader>to",
+				"<cmd>lua require('neotest').output_panel.toggle()<cr>",
+				desc = "Toggle Test Output Panel",
+			},
+			{ "<leader>tp", "<cmd>lua require('neotest').run.stop()<cr>", desc = "Stop the nearest test" },
+			{ "<leader>ts", "<cmd>lua require('neotest').summary.toggle()<cr>", desc = "Toggle Test Summary" },
+			{ "<leader>tt", "<cmd>lua require('neotest').run.run()<cr>", desc = "Run the nearest test" },
+			{
+				"<leader>tT",
+				"<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
+				desc = "Run test the current file",
+			},
+		},
+		opts = {
+			adapters = {
+				["neotest-go"] = {
+					-- Here we can set options for neotest-go, e.g.
+					-- args = { "-tags=integration" }
+				},
+			},
+		},
+	},
 }
