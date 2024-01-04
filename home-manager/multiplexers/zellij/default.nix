0d884bae7355bccdde675cfcd16d3bdf614a5203
Haseeb Majid <hello@haseebmajid.dev>
2024-01-04 01:04:58 +0000

    feat: add a Zellij statusbar

--- home-manager/multiplexers/zellij/default.nix
+++ home-manager/multiplexers/zellij/default.nix
@@ -20,25 +20,35 @@ in {
     ];
     xdg.configFile = {
       "zellij/config.kdl".source = ./config.kdl;
-      #   "zellij/layouts/default.kdl".text = ''
-      #     layout {
-      #       pane size=1 borderless=true {
-      #         plugin location="file:${inputs.zjstatus.packages.${pkgs.system}.default}/bin/zjstatus.wasm" {
-      #           format_left "{mode}#[bg=${colors.base04}] {tabs}"
-      #           format_space "#[bg=${colors.base04}]"
-      #
-      #           mode_normal "#[bg=${colors.base02},fg=${colors.base07},bold] {name} "
-      #           mode_tab "#[bg=${colors.base04},fg=${colors.base00},bold] {name} "
-      #
-      #           tab_normal "#[bg=${colors.base02},fg=${colors.base0C}] {name} "
-      #           tab_active "#[bg=${colors.base03},fg=${colors.base00}] {name} "
-      #         }
-      #       }
-      #       pane split_direction="vertical" {
-      #         pane
-      #       }
-      #     }
-      #   '';
+      "zellij/layouts/zjstatus.kdl".text = ''
+        layout {
+         tab {
+        	 pane
+         }
+
+         default_tab_template {
+        	 pane size=1 borderless=true {
+        		 plugin location="file:${inputs.zjstatus.packages.${pkgs.system}.default}/bin/zjstatus.wasm" {
+        			 format_left "{tabs}"
+        			 format_right "{mode}#[bg=#f5c2e7,fg=#000000,bold]  #[bg=#313244,bold] {session} "
+
+        			 mode_normal        "#[fg=#b8bb26,bold]{name}"
+        			 mode_locked        "#[fg=#fb4934,bold]{name}"
+        			 mode_resize        "#[fg=#fabd2f,bold]{name}"
+        			 mode_pane          "#[fg=#d3869b,bold]{name}"
+        			 mode_tab           "#[fg=#83a598,bold]{name}"
+        			 mode_scroll        "#[fg=#8ec07c,bold]{name}"
+        			 mode_session       "#[fg=#fe8019,bold]{name}"
+        			 mode_move          "#[fg=#a89984,bold]{name}"
+
+        			 tab_normal   "#[fg=#fab387,bold] {name}"
+        			 tab_active   "#[fg=#89b4fa,bold] {name}"
+        		 }
+        	 }
+        	 children
+         }
+        }
+      '';
     };
 
     programs.zellij = {
