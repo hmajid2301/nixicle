db4e7f94c6542508ee42a2798422616305d2c051
Haseeb Majid <hello@haseebmajid.dev>
2024-03-16 18:10:13 +0000

    fix: more outdates options in config

--- home-manager/programs/gpg/default.nix
+++ home-manager/programs/gpg/default.nix
@@ -15,7 +15,7 @@
     enableSshSupport = true;
     enableExtraSocket = true;
     sshKeys = ["D528D50F4E9F031AACB1F7A9833E49C848D6C90"];
-    pinentryFlavor = "gnome3";
+    pinentryPackage = "gnome3";
   };
 
   programs = {
