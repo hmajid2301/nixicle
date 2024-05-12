a714cab84ca39f06886c75919e5811d1f1f7f860
Haseeb Majid <haseeb.majid@imaginecurve.com>
2024-05-12 10:41:53 +0100

    feat: update lock file

--- modules/home/cli/editors/nvim/plugins/lang/svelte.nix
+++ modules/home/cli/editors/nvim/plugins/lang/svelte.nix
@@ -15,39 +15,13 @@
     };
 
     plugins = {
-      # conform-nvim = {
-      #   formattersByFt = {
-      #     go = ["goimports"];
-      #   };
-      #
-      #   formatters = {
-      #     goimports = {
-      #       command = "${pkgs.gotools}/bin/goimports";
-      #       args = [
-      #         "-local"
-      #         "gitlab.com/majiy00,gitlab.com/hmajid2301"
-      #       ];
-      #     };
-      #   };
-      # };
-      #
-      # lint = {
-      #   lintersByFt = {
-      #     go = ["golangcilint"];
-      #   };
-      #   linters = {
-      #     golangcilint = {
-      #       cmd = "${pkgs.golangci-lint}/bin/golangci-lint";
-      #     };
-      #   };
-      # };
-
       lsp.servers.svelte = {
         enable = true;
       };
 
       lsp.servers.tailwindcss = {
         enable = true;
+        filetypes = ["svelte"];
       };
 
       treesitter = {
