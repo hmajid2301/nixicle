e0f72fc629dd71407a17b036d61deb9232370a58
Haseeb Majid <hello@haseebmajid.dev>
2024-09-28 20:38:43 +0100

    work

--- containers/ci.nix
+++ containers/ci.nix
@@ -25,7 +25,7 @@ pkgs.dockerTools.buildImage {
       "NIX_PAGER=cat"
       # A user is required by nix
       # https://github.com/NixOS/nix/blob/9348f9291e5d9e4ba3c4347ea1b235640f54fd79/src/libutil/util.cc#L478
-      "USER=nobody"
+      "USER=nixbld"
       "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
       "SSL_CERT_DIR=${pkgs.cacert}/etc/ssl/certs/"
     ];
