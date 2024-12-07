0a728e88f983aa11f0a79373b69de24c687b0a55
Haseeb Majid <hello@haseebmajid.dev>
2024-12-07 14:00:06 +0000

    add: postgres auth

--- modules/nixos/services/postgres/default.nix
+++ modules/nixos/services/postgres/default.nix
@@ -18,6 +18,15 @@ in {
         # TODO: look at using default postgres
         package = pkgs.postgresql_16_jit;
         extraPlugins = ps: with ps; [pgvecto-rs];
+        authentication = pkgs.lib.mkOverride 10 ''
+          #...
+          #type database DBuser origin-address auth-method
+          local all       all     trust
+          # ipv4
+          host  all      all     127.0.0.1/32   trust
+          # ipv6
+          host all       all     ::1/128        trust
+        '';
         settings = {
           shared_preload_libraries = ["vectors.so"];
           search_path = "\"$user\", public, vectors";
