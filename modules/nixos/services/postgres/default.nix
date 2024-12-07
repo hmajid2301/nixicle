c9d714f82e68f03d41989b3b303628d30e294868
Haseeb Majid <hello@haseebmajid.dev>
2024-12-07 13:44:05 +0000

    work

--- modules/nixos/services/postgres/default.nix
+++ modules/nixos/services/postgres/default.nix
@@ -18,16 +18,52 @@ in {
         # TODO: look at using default postgres
         package = pkgs.postgresql_16_jit;
         extraPlugins = ps: with ps; [pgvecto-rs];
+        authentication = pkgs.lib.mkOverride 10 ''
+          #...
+          #type database DBuser origin-address auth-method
+          local all       all     trust
+          # ipv4
+          host  all      all     127.0.0.1/32   trust
+          # ipv6
+          host all       all     ::1/128        trust
+        '';
         settings = {
           shared_preload_libraries = ["vectors.so"];
           search_path = "\"$user\", public, vectors";
         };
       };
+
       postgresqlBackup = {
         enable = true;
-        location = "/mnt/share/postgresql";
+        # location = "/mnt/share/postgresql";
         backupAll = true;
         startAt = "*-*-* 10:00:00";
+      };
+
+      traefik = {
+        dynamicConfigOptions = {
+          tcp = {
+            services = {
+              postgres = {
+                loadBalancer = {
+                  servers = [
+                    {
+                      address = "127.0.0.1:5432";
+                    }
+                  ];
+                };
+              };
+            };
+
+            routers = {
+              postgres = {
+                entryPoints = ["postgres"];
+                rule = "HostSNI(`*`)";
+                service = "postgres";
+              };
+            };
+          };
+        };
       };
     };
   };
