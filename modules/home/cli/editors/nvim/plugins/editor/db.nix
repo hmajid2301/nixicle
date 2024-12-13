9ce4d9349b046867bdf0ba899057a2a1ba3d7b3c
Haseeb Majid <hello@haseebmajid.dev>
2024-12-13 23:19:31 +0000

    work

--- modules/home/cli/editors/nvim/plugins/editor/db.nix
+++ modules/home/cli/editors/nvim/plugins/editor/db.nix
@@ -7,5 +7,19 @@
     extraConfigLua = ''
       require("dbee").setup()
     '';
+
+    plugins.lsp.servers.sqls = {
+      enable = true;
+      settings = {
+        sqls = {
+          connections = [
+            {
+              driver = "postgresql";
+              dataSourceName = "host=127.0.0.1 port=5432 user=postgres password=postgres dbname=postgres sslmode=disable";
+            }
+          ];
+        };
+      };
+    };
   };
 }
