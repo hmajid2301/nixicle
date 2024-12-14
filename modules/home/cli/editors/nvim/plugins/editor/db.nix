d0d3992b552681a2c542de25bd4169799dde24e9
Haseeb Majid <hello@haseebmajid.dev>
2024-12-14 09:46:13 +0000

    work

--- modules/home/cli/editors/nvim/plugins/editor/db.nix
+++ modules/home/cli/editors/nvim/plugins/editor/db.nix
@@ -1,15 +1,36 @@
 {pkgs, ...}: {
   programs.nixvim = {
-    plugins.lsp.servers.sqls = {
-      enable = true;
-      settings = {
-        sqls = {
-          connections = [
-            {
-              driver = "postgresql";
-              dataSourceName = "host=127.0.0.1 port=5432 user=postgres password=postgres dbname=postgres sslmode=disable";
-            }
-          ];
+    plugins.lsp.servers = {
+      sqlls = {
+        enable = true;
+        package = pkgs.nixicle.sql-language-server;
+        settings = {
+          sqlLanguageServer = {
+            connections = [
+              {
+                name = "postgres";
+                adapter = "postgres";
+                host = "localhost";
+                port = 5432;
+                user = "postgres";
+                password = "postgres";
+                database = "postgres";
+              }
+            ];
+          };
+        };
+      };
+      sqls = {
+        enable = true;
+        settings = {
+          sqls = {
+            connections = [
+              {
+                driver = "postgresql";
+                dataSourceName = "host=127.0.0.1 port=5432 user=postgres password=postgres dbname=postgres sslmode=disable";
+              }
+            ];
+          };
         };
       };
     };
