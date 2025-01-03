5a018844c310f0bcfac2c2286589e4befe5e2416
Haseeb Majid <haseeb.majid@imaginecurve.com>
2025-01-03 12:14:46 +0000

    work

--- modules/nixos/services/monitoring/default.nix
+++ modules/nixos/services/monitoring/default.nix
@@ -173,6 +173,41 @@ in {
           }
 
           {
+            job_name = "redis";
+            static_configs = [
+              {
+                targets = [
+                  "127.0.0.1:${toString config.services.prometheus.exporters.redis.port}"
+                ];
+              }
+            ];
+          }
+
+          {
+            job_name = "postgres";
+            static_configs = [
+              {
+                targets = [
+                  "127.0.0.1:${toString config.services.prometheus.exporters.postgres.port}"
+                ];
+              }
+            ];
+          }
+
+          {
+            job_name = "minio";
+            metrics_path = "/minio/metrics/v3";
+            bearer_token_file = config.sops.secrets.minio_prometheus_bearer_token.path;
+            static_configs = [
+              {
+                targets = [
+                  "127.0.0.1${toString config.services.minio.listenAddress}"
+                ];
+              }
+            ];
+          }
+
+          {
             job_name = "nodes";
             static_configs = [
               {
