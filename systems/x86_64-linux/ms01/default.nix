d230fb455b2e27665a927e12a0765ac106c8fce9
Haseeb Majid <hello@haseebmajid.dev>
2025-02-08 21:45:50 +0000

    feat: add ollama and webui

--- systems/x86_64-linux/ms01/default.nix
+++ systems/x86_64-linux/ms01/default.nix
@@ -35,35 +35,7 @@
       };
     };
 
-    samba-wsdd = {
-      enable = true;
-      openFirewall = true;
-    };
-
-    samba = {
-      enable = true;
-      openFirewall = true;
-      nmbd.enable = true;
-      winbindd.enable = true;
-      settings = {
-        global = {
-          "hosts allow" = "192.168.1. 100.64.0.0/10 127.0.0.1 localhost";
-          "bind interfaces only" = "yes";
-          interfaces = "lo enp90s0 tailscale0";
-          security = "user";
-          "min protocol" = "SMB2";
-          "browseable" = "yes";
-          "guest ok" = "yes";
-        };
-        public = {
-          "path" = "/mnt/n1";
-          "guest ok" = "yes";
-          "read only" = "no";
-          "create mask" = "0755";
-          "directory mask" = "0755";
-        };
-      };
-    };
+    ollama.acceleration = lib.mkForce "cuda";
   };
 
   services = {
--- systems/x86_64-linux/workstation/default.nix
+++ systems/x86_64-linux/workstation/default.nix
@@ -13,6 +13,13 @@
 
   environment.pathsToLink = ["/share/fish"];
 
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      rocmPackages.clr.icd
+    ];
+  };
+
   # TODO: when merged in
   systemd.package = pkgs.systemd.overrideAttrs (old: {
     patches =
