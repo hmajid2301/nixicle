804b7cc57ddf694d8d06644bbd1546da36f324c6
Haseeb Majid <hello@haseebmajid.dev>
2025-03-01 14:12:40 +0000

    work

--- modules/home/cli/editors/neovim/lua/myLuaConf/plugins/init.lua
+++ modules/home/cli/editors/neovim/lua/myLuaConf/plugins/init.lua
@@ -10,10 +10,6 @@
 -- put this in your main init.lua file ( before lazy setup )
 vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46_cache/"
 
-for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
-	dofile(vim.g.base46_cache .. v)
-end
-
 -- require("catppuccin").setup({
 -- 	flavour = "mocha",
 -- 	integrations = {
