070060c6cd5829864a14995ecf92abb1a361b95e
Haseeb Majid <hello@haseebmajid.dev>
2025-06-12 20:11:20 +0100

    work

--- modules/home/roles/development/default.nix
+++ modules/home/roles/development/default.nix
@@ -36,19 +36,19 @@ in
     };
 
     cli = {
-      # multiplexers.zellij.enable = true;
+      multiplexers.zellij.enable = true;
 
       programs = {
-        # attic.enable = true;
-        # atuin.enable = true;
-        # bat.enable = true;
-        # bottom.enable = true;
-        # db.enable = true;
-        # direnv.enable = true;
-        # eza.enable = true;
-        # fzf.enable = true;
-        # git.enable = true;
-        # gpg.enable = true;
+        attic.enable = true;
+        atuin.enable = true;
+        bat.enable = true;
+        bottom.enable = true;
+        db.enable = true;
+        direnv.enable = true;
+        eza.enable = true;
+        fzf.enable = true;
+        git.enable = true;
+        gpg.enable = true;
         # htop.enable = true;
         # k8s.enable = true;
         # modern-unix.enable = true;
