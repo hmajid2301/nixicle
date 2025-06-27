96162d6530206f8841d84d4c943c7ee4d04c6806
Haseeb Majid <hello@haseebmajid.dev>
2025-06-27 17:48:28 +0100

    more work?

--- modules/home/cli/editors/neovim/lua/myLuaConf/plugins/completion.lua
+++ modules/home/cli/editors/neovim/lua/myLuaConf/plugins/completion.lua
@@ -93,6 +93,7 @@ return {
 				snippets = { preset = "luasnip" },
 				signature = { enabled = true },
 				cmdline = {
+					enabled = true,
 					-- keymap = {
 					-- 	preset = "inherit",
 					-- 	["<cr>"] = { "select_and_accept" },
@@ -101,9 +102,8 @@ return {
 					-- 	["<C-e>"] = { "cancel" },
 					-- },
 					-- completion = { menu = { auto_show = true } },
-
-					keymap = { preset = "inherit" },
-					completion = { menu = { auto_show = true } },
+					-- keymap = { preset = "inherit" },
+					-- completion = { menu = { auto_show = true } },
 				},
 				completion = {
 					menu = {
