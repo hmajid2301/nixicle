0c8ead6550a5e9b631ab8d4bd9733e9db1f9a9ab
Haseeb Majid <hello@haseebmajid.dev>
2025-07-24 22:08:45 +0100

    work

--- modules/home/cli/programs/attic/default.nix
+++ modules/home/cli/programs/attic/default.nix
@@ -5,9 +5,11 @@
   ...
 }:
 with lib;
-with lib.nixicle; let
+with lib.nixicle;
+let
   cfg = config.cli.programs.attic;
-in {
+in
+{
   options.cli.programs.attic = with types; {
     enable = mkBoolOpt false "Whether or not to enable attic";
   };
@@ -24,11 +26,11 @@ in {
     nix.settings = {
       trusted-substituters = [
         "https://staging.attic.rs/attic-ci"
-        "https://majiy00-nix-binary-cache.fly.dev/system?priority=43"
+        "https://attic.homelab.haseebmajid.dev/main"
       ];
       trusted-public-keys = [
         "attic-ci:U5Sey4mUxwBXM3iFapmP0/ogODXywKLRNgRPQpEXxbo="
-        "system:DdaMnHcRKtgaov3GCR8mlrFuX90ShC2LkHv6kC7nluo="
+        "main:VlacPrGj7LVuEavaWpEgun9eCNvB6DPqYMe3FraKGzw="
       ];
       netrc-file = config.sops.secrets."netrc".path;
     };
