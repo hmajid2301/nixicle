e6a45d414d1dd71d398f11af5364933838b0be42
Haseeb Majid <hello@haseebmajid.dev>
2025-08-01 21:31:15 +0100

    work

--- systems/x86_64-linux/workstation/default.nix
+++ systems/x86_64-linux/workstation/default.nix
@@ -16,6 +16,7 @@
 
   environment.systemPackages = with pkgs; [
     inputs.caelestia.packages.${pkgs.system}.default
+    inputs.caelestia.inputs.caelestia-cli.packages.${pkgs.system}.default
   ];
 
   programs.neovim = {
