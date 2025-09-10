8d583510c76e44e6e5df67e104806e7e6c3be8a3
Haseeb Majid <hello@haseebmajid.dev>
2025-09-10 08:29:48 +0100

    ci: push to attic from build:flake job

--- systems/x86_64-linux/ms01/default.nix
+++ systems/x86_64-linux/ms01/default.nix
@@ -68,7 +68,7 @@
       netdata.enable = true;
       #nfs.enable = true;
       # smb.enable = true;
-      paperless.enable = true;
+      # paperless.enable = true;
       postgresql.enable = true;
       redis.enable = true;
       traefik.enable = true;
