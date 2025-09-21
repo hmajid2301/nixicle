1d3650b6bc4a9f7489838b4ea60e53200e00c676
Haseeb Majid <hello@haseebmajid.dev>
2025-09-21 23:01:31 +0100

    work

--- modules/nixos/services/backup/default.nix
+++ modules/nixos/services/backup/default.nix
@@ -8,6 +8,10 @@ let
   cfg = config.services.backup;
 in
 {
+  imports = [
+    ../s3-backup
+  ];
+
   options.services.backup = {
     enable = mkEnableOption "Enable cloud backups";
   };
@@ -19,10 +23,7 @@ in
       environmentFile = config.sops.secrets.restic_env.path;
 
       # TODO: use username here
-      paths = [
-        "/home/haseeb"
-        "/var/lib/postgresql"
-      ];
+      paths = [ "/home/haseeb" ];
       repository = "b2:Majiy00Backup";
       timerConfig = {
         OnUnitActiveSec = "1d";
