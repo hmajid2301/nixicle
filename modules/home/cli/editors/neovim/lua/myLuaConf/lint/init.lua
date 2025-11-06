18f70b23fde4cd073a2045def7054d388db17000
Haseeb Majid <hello@haseebmajid.dev>
2025-11-06 17:11:44 +0000

    fix: handle golangcilint exit codes properly
    Set --issues-exit-code=0 to prevent linter from exiting with error when issues are found

--- modules/home/cli/editors/neovim/lua/myLuaConf/lint/init.lua
+++ modules/home/cli/editors/neovim/lua/myLuaConf/lint/init.lua
@@ -4,7 +4,17 @@ require("lze").load({
 		for_cat = "lint",
 		event = "FileType",
 		after = function(plugin)
-			require("lint").linters_by_ft = {
+			local lint = require("lint")
+
+			-- Customize golangci-lint to handle exit codes properly
+			-- Exit code 3 means issues found, which is not an error
+			lint.linters.golangcilint.args = {
+				"run",
+				"--out-format=json",
+				"--issues-exit-code=0", -- Don't exit with error when issues found
+			}
+
+			lint.linters_by_ft = {
 				docker = { "hadolint" },
 				go = { "golangcilint" },
 				html = { "htmlhint" },
