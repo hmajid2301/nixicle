3c94115dd332d9275a8b02bbb6006b80d867793b
Haseeb Majid <hello@haseebmajid.dev>
2025-11-08 23:29:37 +0000

    feat: enable Nautilus thumbnails for remote/network files

--- modules/nixos/roles/desktop/addons/nautilus/default.nix
+++ modules/nixos/roles/desktop/addons/nautilus/default.nix
@@ -1,8 +1,15 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib;
 with lib.nixicle;
-let cfg = config.roles.desktop.addons.nautilus;
-in {
+let
+  cfg = config.roles.desktop.addons.nautilus;
+in
+{
   options.roles.desktop.addons.nautilus = with types; {
     enable = mkBoolOpt false "Whether to enable the gnome file manager.";
   };
@@ -13,18 +20,17 @@ in {
 
     environment = {
       sessionVariables = {
-        NAUTILUS_EXTENSION_DIR =
-          "${config.system.path}/lib/nautilus/extensions-4";
-        NAUTILUS_4_EXTENSION_DIR =
-          "${config.system.path}/lib/nautilus/extensions-4";
-        GST_PLUGIN_SYSTEM_PATH_1_0 =
-          lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0"
-          (with pkgs.gst_all_1; [
+        NAUTILUS_EXTENSION_DIR = "${config.system.path}/lib/nautilus/extensions-4";
+        NAUTILUS_4_EXTENSION_DIR = "${config.system.path}/lib/nautilus/extensions-4";
+        GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (
+          with pkgs.gst_all_1;
+          [
             gst-plugins-good
             gst-plugins-bad
             gst-plugins-ugly
             gst-libav
-          ]);
+          ]
+        );
       };
 
       pathsToLink = [ "/share/nautilus-python/extensions" ];
@@ -42,13 +48,13 @@ in {
     snowfallorg.users.${config.user.name}.home.config = {
       dconf.settings = {
         "org/gnome/nautilus/preferences" = {
-          show-image-thumbnails = "always"; # Show thumbnails for all locations (local + remote)
-          thumbnail-limit = 10; # Maximum allowed value (0-10 range)  
+          show-image-thumbnails = "always";
+          thumbnail-limit = 10;
           show-directory-item-counts = "always";
           executable-text-activation = "ask";
           always-use-location-entry = false;
           default-folder-viewer = "icon-view";
-          thumbnail-cache-time = 30; # Cache thumbnails for 30 days
+          thumbnail-cache-time = 30;
         };
         "org/gnome/desktop/thumbnailers" = {
           disable-all = false;
@@ -63,7 +69,8 @@ in {
         "org/gnome/desktop/thumbnailers/ffmpegthumbnailer" = {
           enable = true;
         };
-        "org/gnome/desktop/privacy" = { 
+
+        "org/gnome/desktop/privacy" = {
           remember-recent-files = false;
           disable-camera = false;
           disable-microphone = false;
