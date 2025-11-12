2bd595099f1753d1494e8f287a4a3da9d298ee56
Haseeb Majid <hello@haseebmajid.dev>
2025-11-12 20:31:34 +0000

    feat: add branch_name utility function

--- modules/home/cli/editors/neovim/lua/config/utils.lua
+++ modules/home/cli/editors/neovim/lua/config/utils.lua
@@ -1,3 +1,27 @@
 local M = {}
 
+--- Get the branch name with git-dir and worktree support
+--- @param worktree table<string, string>|nil # a table specifying the `toplevel` and `gitdir` of a worktree
+--- @param as_path string|nil # execute the git command from specific path
+--- @return string branch # The branch name
+function M.branch_name(worktree, as_path)
+	local branch
+
+	if worktree then
+		branch = vim.fn.system(
+			string.format(
+				"git --git-dir=%s --work-tree=%s branch --show-current 2> /dev/null | tr -d '\n'",
+				worktree.gitdir,
+				worktree.toplevel
+			)
+		)
+	elseif as_path then
+		branch = vim.fn.system(string.format("git -C %s branch --show-current 2> /dev/null | tr -d '\n'", as_path))
+	else
+		branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+	end
+
+	return branch
+end
+
 return M
