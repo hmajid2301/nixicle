3b457bf288078ffbcb82a456de5821ad7dcfdabf
Haseeb Majid <hello@haseebmajid.dev>
2025-11-22 23:24:31 +0000

    feat: add shared nix cache configuration module

--- modules/nixos/system/nix/default.nix
+++ modules/nixos/system/nix/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  inputs,
 mkOpt ? null,
 mkBoolOpt ? null,
 enabled ? null,
@@ -26,7 +27,27 @@ in {
         system-features = ["kvm" "big-parallel" "nixos-test"];
         # Disable global flake registry (old URL is 404)
         flake-registry = "";
+
+        # Binary caches
+        substituters = [
+          "https://cache.nixos.org"
+          "https://nix-community.cachix.org"
+          "https://numtide.cachix.org"
+          "https://nvim-treesitter-main.cachix.org"
+          "https://hyprland.cachix.org"
+          "https://niri.cachix.org"
+        ];
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+          "nvim-treesitter-main.cachix.org-1:cbwE6blfW5+BkXXyeAXoVSu1gliqPLHo2m98E4hWfZQ="
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+        ];
       };
+
+      # Add nixpkgs to local registry so 'nix shell nixpkgs#' works
+      registry.nixpkgs.flake = inputs.nixpkgs;
 
       gc = {
         automatic = lib.mkDefault true;
