0ea1a8bc218ce442d3cd25d0998ef21a73f9cf56
Haseeb Majid <hello@haseebmajid.dev>
2025-11-22 23:24:32 +0000

    refactor: move hyprland cache config to shared nix module

--- modules/home/desktops/hyprland/default.nix
+++ modules/home/desktops/hyprland/default.nix
@@ -1,6 +1,7 @@
-{ 
-  config, 
+{
+  config,
   lib,
+  pkgs,
   mkOpt ? null,
   ...
 }:
@@ -23,13 +24,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    nix.settings = {
-      trusted-substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      ];
-    };
-
     desktops.addons = {
       kanshi.enable = true;
       rofi.enable = true;
