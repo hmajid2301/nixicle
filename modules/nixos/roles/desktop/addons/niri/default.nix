831fdebbf0426d5564871e5e2d3c02f6e916f71a
Haseeb Majid <hello@haseebmajid.dev>
2025-11-22 23:24:34 +0000

    feat: add niri window manager nixos module

---
+++ modules/nixos/roles/desktop/addons/niri/default.nix
@@ -0,0 +1,55 @@
+{
+  config,
+  lib,
+  pkgs,
+  mkBoolOpt ? null,
+  ...
+}:
+with lib;
+
+let
+  cfg = config.roles.desktop.addons.niri;
+in
+{
+  options.roles.desktop.addons.niri = with types; {
+    enable = mkBoolOpt false "Enable or disable the niri window manager.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.niri = {
+      enable = true;
+      package = pkgs.niri-unstable.overrideAttrs (old: {
+        doCheck = false;
+      });
+    };
+
+    environment.sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+    };
+
+    environment.systemPackages = with pkgs; [
+      wl-clipboard
+      slurp
+      grim
+      wf-recorder
+      brightnessctl
+    ];
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-gnome
+      ];
+      config.niri = {
+        default = [ "gtk" ];
+        "org.freedesktop.impl.portal.Screencast" = [ "gnome" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+      };
+    };
+
+    roles.desktop.addons.greetd.enable = true;
+    programs.xwayland.enable = true;
+    security.polkit.enable = true;
+  };
+}
