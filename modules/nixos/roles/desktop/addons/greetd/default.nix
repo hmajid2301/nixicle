3f13369fce8444b8bbe4794d2ba02f095c03e3ab
Haseeb Majid <hello@haseebmajid.dev>
2025-11-22 23:24:38 +0000

    feat: add auto-detection for window manager in greetd

--- modules/nixos/roles/desktop/addons/greetd/default.nix
+++ modules/nixos/roles/desktop/addons/greetd/default.nix
@@ -1,13 +1,29 @@
 {
   config,
   lib,
+  mkOpt ? null,
   ...
 }:
-with lib; let
+with lib; 
+let
   cfg = config.roles.desktop.addons.greetd;
+  
+  # Auto-detect which window manager is enabled
+  # Priority: niri > hyprland > gnome
+  defaultCommand = 
+    if config.roles.desktop.addons.niri.enable or false then
+      "niri-session &> /dev/null"
+    else if config.roles.desktop.addons.hyprland.enable or false then
+      "Hyprland &> /dev/null"
+    else if config.roles.desktop.addons.gnome.enable or false then
+      "gnome-session"
+    else
+      "Hyprland &> /dev/null";  # Fallback to Hyprland
 in {
-  options.roles.desktop.addons.greetd = {
+  options.roles.desktop.addons.greetd = with types; {
     enable = mkEnableOption "Enable login greeter";
+    
+    command = mkOpt str defaultCommand "Command to run on login. Auto-detected based on enabled window managers.";
   };
 
   config = mkIf cfg.enable {
@@ -15,7 +31,7 @@ in {
       enable = true;
       settings = rec {
         default_session = {
-          command = "Hyprland &> /dev/null";
+          command = cfg.command;
           user = config.user.name;
         };
         initial_session = default_session;
