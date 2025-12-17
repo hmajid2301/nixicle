04153bbfc208fe899f91e5d94d7aaa7261965748
Haseeb Majid <hello@haseebmajid.dev>
2025-12-17 20:49:22 +0000

    feat(iso): update graphical ISO configuration

--- iso/graphical/default.nix
+++ iso/graphical/default.nix
@@ -1,38 +1,59 @@
-{lib, ...}: {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: {
+  # Boot configuration for ISO
+  boot.loader.systemd-boot.enable = lib.mkDefault true;
+  boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
 
+  # Network configuration
   networking.wireless.enable = lib.mkForce false;
-  hardware.networking.enable = true;
+  networking.networkmanager.enable = true;
 
-  roles = {
-    desktop.addons.gnome.enable = true;
-  };
+  # Enable SSH using our custom SSH module
+  services.ssh.enable = true;
 
-  nix.enable = true;
-  services = {
-    openssh.enable = true;
-  };
+  # Basic locale setup for ISO
+  i18n.defaultLocale = "en_US.UTF-8";
 
-  system = {
-    locale.enable = true;
-  };
-
+  # Auto-login for convenience
   services.displayManager.autoLogin = {
     enable = true;
     user = "nixos";
   };
 
-  users.users = {
-    nixos.extraGroups = ["networkmanager"];
-
-    # TODO: reuse existing openss config
-    nixos.openssh.authorizedKeys.keys = [
+  # User configuration - only for ISO builds
+  users.users = lib.mkIf false {
+    nixos = {
+      isNormalUser = true;
+      group = "nixos";
+      extraGroups = ["networkmanager"];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKuM4bCeJq0XQ1vd/iNK650Bu3wPVKQTSB0k2gsMKhdE hello@haseebmajid.dev"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINP5gqbEEj+pykK58djSI1vtMtFiaYcygqhHd3mzPbSt hello@haseebmajid.dev"
+      ];
+    };
+    root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKuM4bCeJq0XQ1vd/iNK650Bu3wPVKQTSB0k2gsMKhdE hello@haseebmajid.dev"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINP5gqbEEj+pykK58djSI1vtMtFiaYcygqhHd3mzPbSt hello@haseebmajid.dev"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOEtfQ0znAH8QyB4Z5FzRPa9iKkBhuriEpqyfoEkiv+ haseeb.majid@imaginecurve.com"
     ];
   };
 
-  system.stateVersion = "23.11";
+  users.groups = lib.mkIf false {
+    nixos = {};
+  };
+
+  # Essential packages for the live ISO
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    htop
+    wget
+    curl
+    firefox
+  ];
+
+  system.stateVersion = "24.05";
 }
