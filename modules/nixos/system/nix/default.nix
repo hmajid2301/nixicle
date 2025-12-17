96800675b81465d1e8eab25cdc8f4b03771d2186
Haseeb Majid <hello@haseebmajid.dev>
2025-12-17 20:48:58 +0000

    feat(nix): update system nix configuration

--- modules/nixos/system/nix/default.nix
+++ modules/nixos/system/nix/default.nix
@@ -43,12 +43,17 @@ in
           "https://cache.nixos.org"
           "https://nix-community.cachix.org"
           "https://numtide.cachix.org"
+          "https://niri.cachix.org"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+          "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
         ];
+        
+        require-sigs = true;
+        fallback = true;
       };
 
       registry.nixpkgs.flake = inputs.nixpkgs;
