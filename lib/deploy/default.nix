553f02ce848c8ff4d447b923f85bb66f85ed198b
Haseeb Majid <hello@haseebmajid.dev>
2025-12-27 16:43:01 +0000

    feat(deploy): add gum-based deploy task

--- lib/deploy/default.nix
+++ lib/deploy/default.nix
@@ -27,7 +27,7 @@ in rec {
       (result: name: let
         host = hosts.${name};
         user = host.config.user.name or null;
-        inherit (host.pkgs) system;
+        system = host.pkgs.stdenv.hostPlatform.system;
       in
         result
         // {
