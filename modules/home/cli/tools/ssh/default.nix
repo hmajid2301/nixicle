10db5b723bb6689e8dc554b39eabeed118d90e6c
Haseeb Majid <hello@haseebmajid.dev>
2026-01-30 23:15:43 +0000

    chore(cli-tools): update opencode and ssh configurations

--- modules/home/cli/tools/ssh/default.nix
+++ modules/home/cli/tools/ssh/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 with lib;
@@ -24,11 +25,21 @@ in
     programs.ssh = {
       enable = true;
       enableDefaultConfig = false;
-      matchBlocks."*" = {
-        addKeysToAgent = "yes";
-        identitiesOnly = true;
-        serverAliveInterval = 60;
-        serverAliveCountMax = 3;
+      matchBlocks = {
+        "*" = {
+          addKeysToAgent = "yes";
+          identitiesOnly = true;
+          serverAliveInterval = 60;
+          serverAliveCountMax = 3;
+        };
+
+        # Tangled Git server via Cloudflare tunnel
+        "git.haseebmajid.dev" = {
+          hostname = "git.haseebmajid.dev";
+          user = "git";
+          port = 22;
+          proxyCommand = "${pkgs.cloudflared}/bin/cloudflared access ssh --hostname %h";
+        };
       };
       extraConfig = ''
         KexAlgorithms sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org
