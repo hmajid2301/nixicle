e741c3a7d96e43263e580ddf8b0ff288e277afd4
Haseeb Majid <hello@haseebmajid.dev>
2026-03-18 22:43:18 +0000

    work

--- hosts/framebox/default.nix
+++ hosts/framebox/default.nix
@@ -163,38 +163,38 @@
   networking.hostName = "framebox";
 
   # TODO: refactor this also.
-  services.rpcbind.enable = true;
-  fileSystems."/mnt/homelab" = {
-    device = "truenas:/mnt/main/main-encrypted";
-    fsType = "nfs";
-    options = [
-      "nfsvers=4"
-      "noatime"
-      "nofail"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=60"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "x-systemd.requires=tailscaled.service"
-      "x-systemd.after=tailscaled.service"
-    ];
-  };
-
-  fileSystems."/mnt/truenas" = {
-    device = "truenas:/mnt/main/main";
-    fsType = "nfs";
-    options = [
-      "nfsvers=4"
-      "noatime"
-      "nofail"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=60"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "x-systemd.requires=tailscaled.service"
-      "x-systemd.after=tailscaled.service"
-    ];
-  };
+  # services.rpcbind.enable = true;
+  # fileSystems."/mnt/homelab" = {
+  #   device = "truenas:/mnt/main/main-encrypted";
+  #   fsType = "nfs";
+  #   options = [
+  #     "nfsvers=4"
+  #     "noatime"
+  #     "nofail"
+  #     "x-systemd.automount"
+  #     "x-systemd.idle-timeout=60"
+  #     "x-systemd.device-timeout=5s"
+  #     "x-systemd.mount-timeout=5s"
+  #     "x-systemd.requires=tailscaled.service"
+  #     "x-systemd.after=tailscaled.service"
+  #   ];
+  # };
+  #
+  # fileSystems."/mnt/truenas" = {
+  #   device = "truenas:/mnt/main/main";
+  #   fsType = "nfs";
+  #   options = [
+  #     "nfsvers=4"
+  #     "noatime"
+  #     "nofail"
+  #     "x-systemd.automount"
+  #     "x-systemd.idle-timeout=60"
+  #     "x-systemd.device-timeout=5s"
+  #     "x-systemd.mount-timeout=5s"
+  #     "x-systemd.requires=tailscaled.service"
+  #     "x-systemd.after=tailscaled.service"
+  #   ];
+  # };
 
   system.stateVersion = "24.05";
 }
