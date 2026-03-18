b65b5b8032f8f2fbffd8ef2c0db10e71abc14301
Haseeb Majid <hello@haseebmajid.dev>
2026-03-18 22:47:39 +0000

    work

--- hosts/framebox/default.nix
+++ hosts/framebox/default.nix
@@ -48,106 +48,106 @@
   };
 
   services = {
-    power-profiles-daemon.enable = true;
-    virtualisation.kvm.enable = true;
-    nixicle = {
-      authentik.enable = true;
-      atuin.enable = true;
-      atticd.enable = true;
-      banterbus = {
-        enable = true;
-        instances = {
-          dev = {
-            port = 8084;
-            domain = "dev.banterbus.games";
-          };
-          prod = {
-            port = 8083;
-            domain = "banterbus.games";
-          };
-        };
-      };
+    # power-profiles-daemon.enable = true;
+    # virtualisation.kvm.enable = true;
+    # nixicle = {
+    #   authentik.enable = true;
+    #   atuin.enable = true;
+    #   atticd.enable = true;
+    #   banterbus = {
+    #     enable = true;
+    #     instances = {
+    #       dev = {
+    #         port = 8084;
+    #         domain = "dev.banterbus.games";
+    #       };
+    #       prod = {
+    #         port = 8083;
+    #         domain = "banterbus.games";
+    #       };
+    #     };
+    #   };
+    #
+    #   # TODO: refactor this out.
+    #   btrbk = {
+    #     enable = true;
+    #     instances.local = {
+    #       onCalendar = "weekly";
+    #       subvolumes = {
+    #         "/persist" = {
+    #           target = "/mnt/truenas/backups/framebox/persist";
+    #           snapshot_dir = ".snapshots";
+    #         };
+    #         "/home" = {
+    #           target = "/mnt/truenas/backups/framebox/home";
+    #           snapshot_dir = ".snapshots";
+    #         };
+    #       };
+    #       retention = {
+    #         weekly = 2;
+    #         monthly = 6;
+    #       };
+    #     };
+    #     backblaze = {
+    #       enable = true;
+    #       bucket = "Majiy00Homelab";
+    #       endpoint = "s3.us-west-004.backblazeb2.com";
+    #       paths = [
+    #         "/persist/.snapshots"
+    #         "/home/.snapshots"
+    #       ];
+    #       onCalendar = "weekly";
+    #     };
+    #   };
+    #
+    #   cloudflare = {
+    #     enable = true;
+    #     tunnelId = "ecef5dbb-834e-43ed-84c6-355a2ac53e59";
+    #     credentialsFile = config.sops.secrets.cloudflared.path;
+    #   };
+    #   crowdsec.enable = true;
+    #   gitea.enable = true;
+    #   goroutinely.enable = true;
+    #   gitlab-runner = {
+    #     enable = true;
+    #     sopsFile = config.sops.secrets.gitlab_runner_env.path;
+    #   };
+    #
+    #   immich = {
+    #     enable = true;
+    #     mediaLocation = "/mnt/homelab/homelab/immich";
+    #   };
+    #
+    #   karakeep.enable = true;
+    #   llama-cpp.enable = true;
+    #   ollama = {
+    #     enable = true;
+    #     acceleration = "vulkan";
+    #     vulkan = {
+    #       flashAttention = true;
+    #       kvCacheType = "q8_0";
+    #       contextLength = 64000;
+    #     };
+    #   };
+    # nixflix.enable = true;
+    # monitoring.enable = true;
+    # open-webui.enable = true;
+    # otel-collector.enable = true;
+    # redis.enable = true;
+    postgresql.enable = true;
+    # paperless = {
+    #   enable = true;
+    #   mediaDir = "/mnt/homelab/homelab/paperless/media";
+    # };
 
-      # TODO: refactor this out.
-      btrbk = {
-        enable = true;
-        instances.local = {
-          onCalendar = "weekly";
-          subvolumes = {
-            "/persist" = {
-              target = "/mnt/truenas/backups/framebox/persist";
-              snapshot_dir = ".snapshots";
-            };
-            "/home" = {
-              target = "/mnt/truenas/backups/framebox/home";
-              snapshot_dir = ".snapshots";
-            };
-          };
-          retention = {
-            weekly = 2;
-            monthly = 6;
-          };
-        };
-        backblaze = {
-          enable = true;
-          bucket = "Majiy00Homelab";
-          endpoint = "s3.us-west-004.backblazeb2.com";
-          paths = [
-            "/persist/.snapshots"
-            "/home/.snapshots"
-          ];
-          onCalendar = "weekly";
-        };
-      };
+    # tangled.enable = true;
+    # tandoor.enable = true;
+    # traefik.enable = true;
+    tailscale.enable = true;
 
-      cloudflare = {
-        enable = true;
-        tunnelId = "ecef5dbb-834e-43ed-84c6-355a2ac53e59";
-        credentialsFile = config.sops.secrets.cloudflared.path;
-      };
-      crowdsec.enable = true;
-      gitea.enable = true;
-      goroutinely.enable = true;
-      gitlab-runner = {
-        enable = true;
-        sopsFile = config.sops.secrets.gitlab_runner_env.path;
-      };
-
-      immich = {
-        enable = true;
-        mediaLocation = "/mnt/homelab/homelab/immich";
-      };
-
-      karakeep.enable = true;
-      llama-cpp.enable = true;
-      ollama = {
-        enable = true;
-        acceleration = "vulkan";
-        vulkan = {
-          flashAttention = true;
-          kvCacheType = "q8_0";
-          contextLength = 64000;
-        };
-      };
-      # nixflix.enable = true;
-      monitoring.enable = true;
-      open-webui.enable = true;
-      otel-collector.enable = true;
-      redis.enable = true;
-      postgresql.enable = true;
-      paperless = {
-        enable = true;
-        mediaDir = "/mnt/homelab/homelab/paperless/media";
-      };
-
-      tangled.enable = true;
-      tandoor.enable = true;
-      traefik.enable = true;
-      tailscale.enable = true;
-
-      # adguard.enable = true;
-      # unbound.enable = true;
-    };
+    # adguard.enable = true;
+    # unbound.enable = true;
+    # };
   };
 
   roles = {
