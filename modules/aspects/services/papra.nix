8a60063d8365429679728e9e1c6c973e85bc17a1
Haseeb Majid <haseeb.majid@nala.money>
2026-07-03 13:00:21 +0100

    feat(service): run papra with native module

--- modules/aspects/services/papra.nix
+++ modules/aspects/services/papra.nix
@@ -1,55 +1,40 @@
 { ... }:
 let
   tunnelId = "ecef5dbb-834e-43ed-84c6-355a2ac53e59";
-  dataDir = "/var/lib/papra";
-  port = 1221;
   domain = "papra.haseebmajid.dev";
+  port = 1221;
 in
 {
   den.aspects.papra = {
     includes = [ ];
     persist.directories = [
       {
-        directory = dataDir;
-        user = "999";
-        group = "999";
+        directory = "/var/lib/papra";
+        user = "papra";
+        group = "papra";
         mode = "0750";
       }
     ];
     nixos =
       { config, ... }:
       {
-        sops.secrets.papra-env.sopsFile = ../../../hosts/framebox/secrets.yaml;
+        sops.secrets.papra-env = {
+                    restartUnits = [ "papra.service" ];
+        };
 
-        virtualisation.oci-containers.backend = "docker";
-
-        systemd.tmpfiles.rules = [
-          "d ${dataDir} 0750 999 999 -"
-          "d ${dataDir}/app-data 0750 999 999 -"
-          "d ${dataDir}/app-data/db 0750 999 999 -"
-          "d ${dataDir}/app-data/documents 0750 999 999 -"
-        ];
-
-        virtualisation.oci-containers.containers.papra = {
-          image = "ghcr.io/papra-hq/papra:latest-rootless";
-          autoStart = true;
-          ports = [ "127.0.0.1:${toString port}:1221" ];
-          volumes = [ "${dataDir}/app-data:/app/app-data" ];
+        services.papra = {
+          enable = true;
+          environmentFile = config.sops.secrets.papra-env.path;
           environment = {
             APP_BASE_URL = "https://${domain}";
             NODE_ENV = "production";
+            SERVER_HOSTNAME = "0.0.0.0";
             DOCUMENT_STORAGE_ENCRYPTION_IS_ENABLED = "true";
+            AUTH_PROVIDERS_EMAIL_IS_ENABLED = "false";
           };
-          environmentFiles = [ config.sops.secrets.papra-env.path ];
-        };
-
-        systemd.services.docker-papra = {
-          after = [ "network-online.target" ];
-          wantedBy = [ "multi-user.target" ];
         };
 
         services.cloudflared.tunnels.${tunnelId}.ingress.${domain} = "http://localhost:${toString port}";
-
       };
   };
 }
